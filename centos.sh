89ff40ecb663085d80cb595aea9d125208f7a4ad
Ronald Tse <ronald.tse@ribose.com>
2022-03-22 16:29:22 +0800

    Add Alpine Linux, add support for Debian, fixes #3

--- centos.sh
+++ centos.sh
@@ -4,14 +4,14 @@ set -e
 
 PLANTUML_URL="${PLANTUML_URL:-http://sourceforge.net/projects/plantuml/files/plantuml.jar/download}"
 
-if [ -f "/opt/plantuml/plantuml.jar" ]; then
+if [[ -f "/opt/plantuml/plantuml.jar" && -f "/usr/bin/plantuml" ]]; then
   echo '[plantuml] PlantUML already installed.'
-else
-  echo '[plantuml] Installing PlantUML...'
-  yum install -y java-1.8.0-openjdk graphviz
-  mkdir -p /opt/plantuml && \
-    curl -o /opt/plantuml/plantuml.jar \
-    -L "${PLANTUML_URL}"
-  printf '#!/bin/sh\nexec java -jar /opt/plantuml/plantuml.jar "$@"' > /usr/bin/plantuml
-  chmod +x /usr/bin/plantuml
+  exit
 fi
+
+echo '[plantuml] Installing PlantUML...'
+yum install -y java-1.8.0-openjdk graphviz
+mkdir -p /opt/plantuml
+curl -o /opt/plantuml/plantuml.jar -L "${PLANTUML_URL}"
+printf '#!/bin/sh\nexec java -Djava.awt.headless=true -jar /opt/plantuml/plantuml.jar "$@"' > /usr/bin/plantuml
+chmod +x /usr/bin/plantuml
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,14 +4,14 @@ set -e
 
 PLANTUML_URL="${PLANTUML_URL:-http://sourceforge.net/projects/plantuml/files/plantuml.jar/download}"
 
-if [ -f "/opt/plantuml/plantuml.jar" ]; then
+if [[ -f "/opt/plantuml/plantuml.jar" && -f "/usr/bin/plantuml" ]]; then
   echo '[plantuml] PlantUML already installed.'
-else
-  echo '[plantuml] Installing PlantUML...'
-  apt-get install -y default-jre graphviz
-  mkdir -p /opt/plantuml && \
-    curl -o /opt/plantuml/plantuml.jar \
-    -L "${PLANTUML_URL}"
-  printf '#!/bin/sh\nexec java -jar /opt/plantuml/plantuml.jar "$@"' > /usr/bin/plantuml
-  chmod +x /usr/bin/plantuml
+  exit
 fi
+
+echo '[plantuml] Installing PlantUML...'
+apt-get install -y default-jre graphviz
+mkdir -p /opt/plantuml
+curl -o /opt/plantuml/plantuml.jar -L "${PLANTUML_URL}"
+printf '#!/bin/sh\nexec java -Djava.awt.headless=true -jar /opt/plantuml/plantuml.jar "$@"' > /usr/bin/plantuml
+chmod +x /usr/bin/plantuml
