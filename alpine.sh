15ec6522d7bb8d69960a08e475087f9eb3ef2995
Alexande B <abobrikovich@gmail.com>
2024-03-25 23:49:59 +0100

    fix: install proper font packages depending on Alpine version

--- alpine.sh
+++ alpine.sh
@@ -4,14 +4,23 @@ set -e
 
 PLANTUML_URL="${PLANTUML_URL:-http://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar}"
 
-if [ -f "/usr/share/java/plantuml.jar" -a -f "/usr/bin/plantuml" ]; then
+if [ -f "/usr/share/java/plantuml.jar" ] && [ -f "/usr/bin/plantuml" ]; then
   echo '[plantuml] PlantUML already installed.'
   exit
 fi
 
 echo '[plantuml] Installing PlantUML...'
-apk add curl graphviz fontconfig
-apk add ttf-droid ttf-droid-nonlatin --repository=http://dl-cdn.alpinelinux.org/alpine/v3.16/community
+apk add curl openjdk8-jre graphviz fontconfig
+
+source /etc/os-release
+ALPINE_MAJOR=$(echo "$VERSION_ID" | cut -d. -f1)
+ALPINE_MINOR=$(echo "$VERSION_ID)" | cut -d. -f2)
+
+if [ "${ALPINE_MAJOR}" -ge 3 ] && [ "${ALPINE_MINOR}" -ge 17 ]; then
+  apk add font-droid font-droid-nonlatin
+else
+  apk add ttf-droid ttf-droid-nonlatin
+fi
 
 mkdir -p /opt/plantuml
 curl -o /opt/plantuml/plantuml.jar -L "${PLANTUML_URL}"
